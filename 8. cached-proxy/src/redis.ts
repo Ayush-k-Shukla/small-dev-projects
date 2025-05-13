f309fa876b554622cdb37998d3a4ba5b0a166867
Ayush Kumar Shukla <shuklaayushkumar27@gmail.com>
2025-05-13 22:45:50 +0530

    feat: add cache basic

--- 8. cached-proxy/src/redis.ts
+++ 8. cached-proxy/src/redis.ts
@@ -14,4 +14,28 @@ class Cache {
       console.log('Error connecting to redis client', err);
     });
   }
+
+  async get<T>(key: string): Promise<T | null> {
+    const d = await this.redisClient.get(key);
+    return d ? JSON.parse(d) : null;
+  }
+
+  async set(key: string, value: any): Promise<void> {
+    await this.redisClient.set(key, JSON.stringify(value), {
+      expiration: {
+        type: 'EX',
+        value: 600
+      }
+    });
+  }
+
+  async clear(key?: string): Promise<void> {
+    if (key) {
+      await this.redisClient.del(key);
+    } else {
+      await this.redisClient.flushAll();
+    }
+  }
 }
+
+export default Cache;
