c9c42a51a7354c6e016a6065bb5fe369dd2be2d8
Ayush Kumar Shukla <shuklaayushkumar27@gmail.com>
2025-11-17 20:31:00 +0530

    feat: lazy image loader

---
+++ 13. Intersection-observer/lazy-image-loader/loader.js
@@ -0,0 +1,39 @@
+const images = [
+  'https://picsum.photos/id/1015/800/600',
+  'https://picsum.photos/id/1024/800/600',
+  'https://picsum.photos/id/1035/800/600',
+  'https://picsum.photos/id/1040/800/600',
+  'https://picsum.photos/id/1057/800/600',
+  'https://picsum.photos/id/1069/800/600',
+  'https://picsum.photos/id/1074/800/600',
+  'https://picsum.photos/id/1084/800/600'
+];
+
+const root = document.getElementById('root');
+
+images.forEach((src) => {
+  const box = document.createElement('div');
+  box.className = 'image-container';
+
+  const img = document.createElement('img');
+  img.dataset.src = src;
+
+  box.appendChild(img);
+  root.appendChild(box);
+});
+
+const observer = new IntersectionObserver((entries, obs) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      const img = entry.target;
+      img.src = img.dataset.src;
+      img.onload = () => {
+        img.classList.add('loaded');
+      };
+
+      obs.unobserve(img);
+    }
+  });
+}, {});
+
+document.querySelectorAll('img').forEach((img) => observer.observe(img));
