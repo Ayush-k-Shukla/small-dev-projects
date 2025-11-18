b93c539ac7345d84ec709c5cf322b93e61acddb0
Ayush Kumar Shukla <shuklaayushkumar27@gmail.com>
2025-11-18 20:50:20 +0530

    fix: loader

--- 13. Intersection-observer/lazy-image-loader/loader.js
+++ 13. Intersection-observer/lazy-image-loader/loader.js
@@ -27,6 +27,8 @@ const observer = new IntersectionObserver((entries, obs) => {
     if (entry.isIntersecting) {
       const img = entry.target;
       img.src = img.dataset.src;
+
+      // or simulate api call
       img.onload = () => {
         img.classList.add('loaded');
       };
